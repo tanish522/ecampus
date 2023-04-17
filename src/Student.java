22ec47f4324c62017381207b0f784353b0589a2b
tanish522 <tanishpatel0502@gmail.com>
2023-04-17 10:46:44 +0530

    student class created v1

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Student {
     private int studId;
     private String name;
+    private ArrayList<Course> courseList = new ArrayList<>();
+    private char overallGrade;
+    private int percentile;
 
+}
+
+class Course{
+    private String courseName;
+    private int totalCredit;
+    private int attainedCredit;
+    private char grades;
 }
 
