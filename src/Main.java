29129796c40a2a91e58312b1280dba0f533408b2
pratish99 <sonipratish32@gmail.com>
2023-04-17 11:10:18 +0530

    Home page v0.1

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         //this do while loop will show the menu to the user to choose from student login, teacher login and exit
         do
         {
-            System.out.print("\n1. Student \n2. Teacher \n3. Exit");
+            System.out.print("\n1. Student Login/register  \n2. Teacher Login/Register \n3. Exit");
             System.out.print("\nEnter your choice: ");
             ch = sc.nextInt();
             //As soon as the user chooses, one of these options will be selected and executed accordingly
@@ -20,11 +20,63 @@ public class Main {
                 case 1:
                 {
                     // student login
+                    int ch2;
+                    do{
+                        System.out.println("\n1. Login \n2. Register \n3.Exit");
+                        ch2 = sc.nextInt();
+                        switch (ch2){
+                            case 1:{
+                                //Call Student Login Method
+                                break;
+                            }
+                            case 2:{
+                                //Call Student Register method
+                                break;
+                            }
+                            case 3:{
+                                System.out.println("Exiting");
+                                break;
+                            }
+                            default:{
+                                System.out.println("Invalid Choice! ");
+                                break;
+                            }
+
+                        }
+                    }while(ch2!=3);
 
                     break;
                 }
                 case 2: {
                     // teacher login
+                    int ch2;
+                    do{
+                        System.out.println("\n1. Login \n2. Register \n3.Exit");
+                        ch2 = sc.nextInt();
+                        switch (ch2){
+                            case 1:{
+                                //Call Teacher Login Method
+                                break;
+                            }
+                            case 2:{
+                                //Call Teacher Register method
+                                break;
+                            }
+                            case 3:{
+                                System.out.println("Exiting");
+                                break;
+                            }
+                            default:{
+                                System.out.println("Invalid Choice! ");
+                                break;
+                            }
+
+                        }
+                    }while(ch2!=3);
+                    break;
+                }
+                case 3:{
+                    System.out.println("Exiting");
                     break;
                 }
                 default:
