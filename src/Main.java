120674f8169676643ed8f4a44d86621ac96a09a7
pratish99 <sonipratish32@gmail.com>
2023-04-23 11:52:52 +0530

    Main Completed

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,11 @@
 import java.io.Console;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private static void init(){
-        HashMap<String,Teacher> teacherList = new HashMap<String,Teacher>();
-        HashMap<Integer,Student> studentList = new HashMap<Integer,Student>();
-
-        Teacher t = new Teacher("amit123","amit123","IT230","data structure",70);
-        teacherList.put(t.getUname(),t);
-        Student s1 = new Student(1,"123","tanish","IT692",100,100,100,"A");
-        studentList.put(s1.getStudId(),s1);
-
-        Teacher.writeToStudentDetails(studentList);
-        Teacher.writeToTeacherDetails(teacherList);
-
-    }
     public static void main(String[] args) throws FileNotFoundException {
 
         init();
@@ -29,176 +17,181 @@ public class Main {
 
         NewSession:
         //this do while loop will show the menu to the user to choose from student login, teacher login and exit
-        do
-        {
-            System.out.print("\n1. Student Login/register  \n2. Teacher Login/Register \n3. Forgot Password \n4. Exit");
-            System.out.print("\nEnter your choice: ");
-            ch = sc.nextInt();
-            //As soon as the user chooses, one of these options will be selected and executed accordingly
-            switch (ch)
-            {
-                case 1:
-                {
-                    // student login
-                    int ch2;
-                        do{
+        try {
+
+
+            do {
+                System.out.print("\n1. Student Login/register  \n2. Teacher Login/Register \n3. Forgot Password \n4. Exit");
+                System.out.print("\nEnter your choice: ");
+                ch = sc.nextInt();
+                //As soon as the user chooses, one of these options will be selected and executed accordingly
+                switch (ch) {
+                    case 1: {
+                        // student login
+                        int ch2;
+                        do {
                             System.out.println("================== Student Login ==================");
                             System.out.println("\n1. Login \n2. Forgot Password\n3. Exit");
 
                             ch2 = sc.nextInt();
-                            switch (ch2){
-                                case 1:{
+                            switch (ch2) {
+                                case 1: {
 
                                     System.out.print("\nEnter StudentId: ");
                                     int studId = sc.nextInt();
                                     String password = hiddenPassword();
                                     Student currstud = new Student();
-                                    if(currstud.loginStud(studentList,studId,password,sc))
-                                    {
+                                    if (currstud.loginStud(studentList, studId, password, sc)) {
                                         currstud = studentList.get(studId);
                                         System.out.println("Login Successful");
                                         currstud.printStudentDetails();
-                                    }
-                                    else
-                                    {
+                                    } else {
                                         System.out.println("Login Failed");
                                     }
                                     break;
                                 }
-                                case 2:{
+                                case 2: {
                                     System.out.println("\nEnter Student ID: ");
                                     int studId = sc.nextInt();
-                                    if (studentList.containsKey(studId)){
+                                    if (studentList.containsKey(studId)) {
                                         Student currStudent = new Student();
                                         currStudent.changePassword(studentList, studId, sc);
-                                    }
-                                    else{
+                                    } else {
                                         System.out.println("\nStudent ID not available! ");
                                     }
 
                                     break;
                                 }
-                                case 3:{
+                                case 3: {
                                     System.out.println("Exiting");
                                     break;
                                 }
-                                default:{
+                                default: {
                                     System.out.println("Invalid Choice! ");
                                     break;
                                 }
 
                             }
-                        }while(ch2!=3);
+                        } while (ch2 != 3);
 
-                    break;
-                }
-                case 2: {
-                    // teacher login
-                    int ch2;
-                    do{
-                        System.out.println("================== Teacher Login ==================");
-                        System.out.println("\n1. Login \n2. Register \n3. Forgot Password\n4. Exit");
+                        break;
+                    }
+                    case 2: {
+                        // teacher login
+                        int ch2;
+                        do {
+                            System.out.println("================== Teacher Login ==================");
+                            System.out.println("\n1. Login \n2. Register \n3. Forgot Password\n4. Exit");
 
-                        ch2 = sc.nextInt();
-                        switch (ch2){
-                            case 1:{
+                            ch2 = sc.nextInt();
+                            switch (ch2) {
+                                case 1: {
 
-                                System.out.print("\nEnter Username: ");
-                                String username = sc.next();
-                                String password = hiddenPassword();
-                                Teacher currteacher = new Teacher();
-                                if(currteacher.loginTeacher(teacherList,username,password))
-                                {
-                                    System.out.println("Login Successful");
-                                    currteacher = teacherList.get(username);
-                                    System.out.println("currteacher "+ currteacher);
-                                    int ch3;
-                                    do{
-                                        System.out.println("\n1. Add Student \n2. Edit Marks \n3. View Students \n4. Delete Student \n5. Logout" );
-                                        System.out.println("\nEnter Your Choice: ");
-                                        ch3 = sc.nextInt();
-                                        switch (ch3){
-                                            case 1:{
-                                                currteacher.addStudent(sc);
-                                                studentList = currteacher.readStudentsFile();
-                                                break;
-                                            }
-                                            case 2:{
-                                                currteacher.editMarks(sc);
-                                                break;
-                                            }
-                                            case 3:{
-                                                currteacher.printStudent(studentList);
-                                                break;
-                                            }
-                                            case 4:{
-                                                currteacher.deleteStudent(sc);
-                                                break;
-                                            }
-                                            case 5:{
-                                                System.out.println("Logout Successful");
-                                                break;
-                                            }
-                                            default:{
+                                    System.out.print("\nEnter Username: ");
+                                    String username = sc.next();
+                                    String password = hiddenPassword();
+                                    Teacher currteacher = new Teacher();
+                                    if (currteacher.loginTeacher(teacherList, username, password)) {
+                                        System.out.println("Login Successful");
+                                        currteacher = teacherList.get(username);
+                                        System.out.println("currteacher " + currteacher);
+                                        int ch3;
+                                        do {
+                                            System.out.println("\n1. Add Student \n2. Edit Marks \n3. View Students \n4. Delete Student \n5. Logout");
+                                            System.out.println("\nEnter Your Choice: ");
+                                            ch3 = sc.nextInt();
+                                            switch (ch3) {
+                                                case 1: {
+                                                    currteacher.addStudent(sc);
+                                                    studentList = currteacher.readStudentsFile();
+                                                    break;
+                                                }
+                                                case 2: {
+                                                    currteacher.editMarks(sc);
+                                                    break;
+                                                }
+                                                case 3: {
+                                                    currteacher.printStudent(studentList);
+                                                    break;
+                                                }
+                                                case 4: {
+                                                    currteacher.deleteStudent(sc);
+                                                    break;
+                                                }
+                                                case 5: {
+                                                    System.out.println("Logout Successful");
+                                                    break;
+                                                }
+                                                default: {
 
-                                                System.out.println("\nEnter Valid Choice! ");
+                                                    System.out.println("\nEnter Valid Choice! ");
+                                                }
                                             }
-                                        }
-                                        studentList = Student.readStudentsFile();
-                                    }while(ch3!=5);
+                                            studentList = Student.readStudentsFile();
+                                        } while (ch3 != 5);
 
+                                    } else {
+                                        System.out.println("Login Failed");
+                                    }
+
+                                    break;
                                 }
-                                else
-                                {
-                                    System.out.println("Login Failed");
+
+                                case 2: {
+                                    Teacher newTeacher = new Teacher();
+                                    newTeacher.registerTeacher(teacherList, sc);  // register teachers
+                                    teacherList.put(newTeacher.getUname(), newTeacher);
+                                    break;
+                                }
+                                case 3: {
+                                    Teacher currteacher = new Teacher();
+                                    System.out.println("\nEnter Username: ");
+                                    String uname = sc.next();
+                                    currteacher.changePassword(teacherList, uname, sc);
+                                    //Call Forgot Password Method
+
+                                    break;
+                                }
+                                case 4: {
+                                    System.out.println("Exiting");
+                                    break;
+                                }
+                                default: {
+                                    System.out.println("Invalid Choice! ");
+                                    break;
                                 }
 
-                                break;
                             }
-
-                            case 2:{
-                                Teacher newTeacher = new Teacher();
-                                newTeacher.registerTeacher(teacherList,sc);  // register teachers
-                                teacherList.put(newTeacher.getUname(), newTeacher);
-                                break;
-                            }
-                            case 3:{
-                                Teacher currteacher = new Teacher();
-                                System.out.println("\nEnter Username: ");
-                                String uname = sc.next();
-                                currteacher.changePassword(teacherList, uname, sc);
-                                //Call Forgot Password Method
-
-                                break;
-                            }
-                            case 4:{
-                                System.out.println("Exiting");
-                                break;
-                            }
-                            default:{
-                                System.out.println("Invalid Choice! ");
-                                break;
-                            }
-
-                        }
-                    }while(ch2!=4);
-                    break;
+                        } while (ch2 != 4);
+                        break;
+                    }
+                    case 3: {
+                        Teacher newTeacher = new Teacher();
+                        newTeacher.registerTeacher(teacherList, sc);
+                    }
+                    case 4: {
+                        System.out.println("Exit");
+                        break;
+                    }
+                    default: {
+                        System.out.println("Enter Valid Choice! ");
+                        break;
+                    }
                 }
-                case 3:{
-                    Teacher newTeacher = new Teacher();
-                    newTeacher.registerTeacher(teacherList,sc);
-                }
-                case 4:{
-                    System.out.println("Exit");
-                    break;
-                }
-                default:
-                {
-                    System.out.println("Enter Valid Choice! ");
-                    break;
-                }
-            }
-        }while(ch!=4);
+            } while (ch != 4);
+        }catch (InputMismatchException e){
+            System.out.println("\n Wrong Input! ");
+        }
+    }
+    private static void init(){
+        HashMap<String,Teacher> teacherList = new HashMap<String,Teacher>();
+        HashMap<Integer,Student> studentList = new HashMap<Integer,Student>();
+        Teacher t = new Teacher("amit123","amit123","IT230","data structure",70);
+        teacherList.put(t.getUname(),t);
+        Student s1 = new Student(1,"123","tanish","IT692",100,100,100,"A");
+        studentList.put(s1.getStudId(),s1);
+        Teacher.writeToStudentDetails(studentList);
+        Teacher.writeToTeacherDetails(teacherList);
     }
     public  static String hiddenPassword(){
          /*
